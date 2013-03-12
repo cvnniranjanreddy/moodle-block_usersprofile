c4a360c360b5e85ee82303bec0275d47f64e8ccc
Niranjan <eabyas@gmail.com>
2013-03-12 11:15:29 -0500

    added the release and maturity  details in the version.php

--- version.php
+++ version.php
@@ -29,3 +29,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2012061700;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012061700;        // Requires this Moodle version
 $plugin->component = 'block_usersprofile'; // Full name of the plugin (used for diagnostics)
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.x (Build: 2012061700)';
